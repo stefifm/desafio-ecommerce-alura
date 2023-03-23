dd27c9112f90d8c404449109d6d4ad5570248eb2
stefifm <bruerastefania@gmail.com>
2023-03-23 11:49:42 -0300

    Add product utils

---
+++ utils/productsList.js
@@ -0,0 +1,33 @@
+import { productService } from '../service/product-service.js'
+
+export const loadProducts = async () => {
+  const products = await productService.productList()
+  return products
+}
+
+export const createLineUserView = (nombre, precio, id, imagen) => {
+  const line = document.createElement('article')
+  line.classList.add('mas-vistos__card')
+
+  const content = `
+
+  <img
+  src="${imagen}"
+  alt="${nombre}"
+  class="mas-vistos__card__img" />
+<div class="mas-vistos__card__details">
+  <h2 class="mas-vistos__card__name">${nombre}</h2>
+  <p class="mas-vistos__card__price">$${precio}</p>
+  <a
+    class="mas-vistos__card__link"
+    href="../screens/descripcion-producto.html?id=${id}"
+    >Ver Producto</a
+  >
+</div>
+  
+  `
+
+  line.innerHTML = content
+
+  return line
+}
